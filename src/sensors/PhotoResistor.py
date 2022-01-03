2bcb7b8e1d913320489dc86a0c1049916b2a1139
Guillaume Chebib <guillaume.chebib@hotmail.fr>
2022-01-03 11:16:24 +0100

    Create PhotoResistor class + export

---
+++ src/sensors/PhotoResistor.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import PCF8591 as ADC
+import RPi.GPIO as GPIO
+import time
+import datetime
+import json
+
+class PhotoResistor():
+    	
+    def __init__(self, analogChannel, digitalChannel):
+        """Initialise a new analog PhotoResistor sensor
+
+        Args:
+            analogChannel (int): the number of the channel of the PCF8591 on which the sensor is pluged on
+            digitalChannel (int): the number of GPIO of the Raspberry PI on which the sensor is pluged on
+
+        Note: use constants from src.constants to simplify the initialisation (see example below)
+        """
+        self.analogChannel = analogChannel
+        self.digitalChannel = digitalChannel
+        GPIO.setup(digitalChannel, GPIO.IN)
+
+    def read(self):
+        """Read the input and return the raw value"""
+        return ADC.read(self.analogChannel) 
+
+
+    def export(self):
+        ts = datetime.datetime.now().timestamp()
+        return json.dumps({"Resistance": self.read(), "Timestamp": ts})
+
+
+
+def setup():
+	ADC.setup(0x48)
+	GPIO.setmode(GPIO.BCM)
+
+
+if __name__ == "__main__":
+    import time
+
+    AIN0 = 0        # Both need to be imported
+    GPIO17 = 17     # from constants when used
+
+
+    def loop():
+        photoResistor = PhotoResistor(AIN0, GPIO17)
+        while True:
+            print(photoResistor.read())
+            time.sleep(1)
+
+    try:
+        setup()
+        loop()
+    except KeyboardInterrupt:
+        pass
