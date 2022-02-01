785408b5bac862724b35a60abcd5886862b7596f
Raphael <raphael.bourret@etu.umontpellier.fr>
2022-02-01 15:29:23 +0100

    improve script

--- setup.sh
+++ setup.sh
@@ -23,17 +23,28 @@ echo "Upgrating apt dependencies..."
 sudo apt -y upgrade >> $LOG_FILE 2>&1   
 
 echo "Installing global dependencies..."
-# sudo apt install >> $LOG_FILE 2>&1
+sudo apt install -y python3-pip >> $LOG_FILE 2>&1
 
 echo "Installing python dependencies..."
-# pip install 
+pip3 install flask wrapt smbus2 Adafruit-BMP persistqueue azure.iot.device >> $LOG_FILE 2>&1
 
 echo "Dependencies installed"
 
-echo "Setting up POE"
-{
-    sudo sh -c 'echo "dtoverlay=dwc2" >> /boot/config.txt'
-    sudo sh -c 'echo "$(cat /boot/cmdline.txt) modules-load=dwc2,g_ether" > /boot/cmdline.txt'
-    sudo touch /boot/ssh
-} >> $LOG_FILE 2>&1
+if ! cat /boot/config.txt | grep -q "dtoverlay=dwc2"; then
+    echo "Setting up POE"
+    {
+        sudo sh -c 'echo "dtoverlay=dwc2" >> /boot/config.txt'
+        sudo sh -c 'echo "$(cat /boot/cmdline.txt) modules-load=dwc2,g_ether" > /boot/cmdline.txt'
+        sudo touch /boot/ssh
+    } >> $LOG_FILE 2>&1
+fi
 
+echo "Setting up API"
+mkdir src/api/cert/keystore -p
+
+openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 \
+    -subj "/C=FR/ST=Herault/L=Montpellier/O=Sopra Steria/CN=raspberrypi" \
+    -keyout src/api/cert/key.pem  -out src/api/cert/cert.pem >> $LOG_FILE 2>&1
+
+echo "Setting up sensors"
+sudo raspi-config nonint do_i2c 0
